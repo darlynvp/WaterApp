69f5da5d330bda317a4f1cad52fae5a7a8edca18
darlynvp <vdarlyn21@gmail.com>
2025-05-12 15:30:13 -0700

    Add files via upload

---
+++ WaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterAppApp.swift
+//  WaterApp
+//
+//  Created by Darlyn Villanueva on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterRemindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
